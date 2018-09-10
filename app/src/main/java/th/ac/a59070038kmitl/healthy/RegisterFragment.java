0634ba208639844dc99c9b63aa2e16e3adb7dc1e
startmt <59070038@it.kmitl.ac.th>
2018-09-10 17:07:05 +0700

    Week5

--- app/src/main/java/th/ac/a59070038kmitl/healthy/RegisterFragment.java
+++ app/src/main/java/th/ac/a59070038kmitl/healthy/RegisterFragment.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterFragment extends Fragment {
     public View onCreateView(
@@ -37,37 +38,50 @@ public class RegisterFragment extends Fragment {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email =  ((EditText)getView().findViewById(R.id.regis_user_email)).getText().toString();
-                String password =  ((EditText)getView().findViewById(R.id.regis_user_password)).getText().toString();
-                String repassword = ((EditText)getView().findViewById(R.id.regis_user_repassword)).getText().toString();
+                String email = ((EditText) getView().findViewById(R.id.regis_user_email)).getText().toString();
+                String password = ((EditText) getView().findViewById(R.id.regis_user_password)).getText().toString();
+                String repassword = ((EditText) getView().findViewById(R.id.regis_user_repassword)).getText().toString();
 
-                if(email.isEmpty() || password.isEmpty() || repassword.isEmpty()){
+                if (email.isEmpty() || password.isEmpty() || repassword.isEmpty()) {
                     Log.d("REGISTER", "FIELD NAME IS EMPTY");
-                    Toast.makeText(getActivity(),"กรุณากรอกข้อมูลให้ครบถ้วน",Toast.LENGTH_SHORT).show();
-                }
-                else if(password.length() < 6 ){
+                    Toast.makeText(getActivity(), "กรุณากรอกข้อมูลให้ครบถ้วน", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 6) {
                     Log.d("REGISTER", "Password not secure");
                     Toast.makeText(getActivity(), "Password ของคุณมีความปลอดภัยต่ำ", Toast.LENGTH_SHORT).show();
-                }
-                else if (password.equals(repassword) == false){
+                } else if (password.equals(repassword) == false) {
                     Log.d("REGISTER", "Password and Re password not match");
                     Toast.makeText(getActivity(), "กรุณากรอก password และ re-password ให้ตรงกัน", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    mAuth.createUserWithEmailAndPassword("email", "password").addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                } else {
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                         @Override
                         public void onSuccess(AuthResult authResult) {
+                            FirebaseUser _user = authResult.getUser();
+                            sendVerifiedEmail(_user);
                             Log.d("REGISTER", "GOTO BMI");
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new BMIFragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new LoginFragment()).commit();
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            Log.d("REGISTER", "WRONG EMAIL");
+                            Log.d("REGISTER", e.getMessage());
                             Toast.makeText(getActivity(), "กรุณากรอก Email ให้ถูกต้อง", Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
+            }
+        });
+    }
+
+    void sendVerifiedEmail(FirebaseUser _user) {
+        _user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
             }
         });
     }
